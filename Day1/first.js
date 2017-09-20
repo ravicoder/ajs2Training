2a036bc4934057b997230de7039c65995e9dc770
ravicoder <srihari.ravi@gmail.com>
2017-09-20 16:44:59 +0530

    Add files via upload

---
+++ Day1/first.js
@@ -0,0 +1,6 @@
+var a = 0;
+var b = 0;
+function print(a) {
+    console.log(a);
+}
+setTimeout(a, 3000);
---
+++ Day1/first.ts
@@ -0,0 +1,8 @@
+let a = 0;
+let b = 0;
+
+function print(a) {
+    console.log(a);
+}
+
+setTimeout(a, 3000);
